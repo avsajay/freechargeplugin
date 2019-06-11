7ca5cc8464e8b8c45f5e9a94d7c05a13cd722efc
avsajay <avsajay490@gmail.com>
2019-06-11 11:33:29 +0530

    add patch to class and modified class code

--- src/android/freechargeplugin.java
+++ src/android/freechargeplugin.java
@@ -6,7 +6,8 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -26,7 +27,18 @@ public class freechargeplugin extends CordovaPlugin {
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);		
+            AlertDialog.Builder builder = new AlertDialog.Builder(cordova.getActivity());
+builder.setMessage("Look at this dialog!")
+       .setCancelable(false)
+       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+           public void onClick(DialogInterface dialog, int id) {
+                //do things
+                 callbackContext.success(message);	
+           }
+       });
+AlertDialog alert = builder.create();
+alert.show();
+           	
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
